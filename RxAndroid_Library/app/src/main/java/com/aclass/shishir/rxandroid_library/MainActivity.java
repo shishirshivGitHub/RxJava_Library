1ac4bec40c921d865892bd1e4c5a958b8fdd6ec7
shishirshiv007 <shishirshiv007@gmail.com>
2017-04-26 17:07:25 +0530

    first commit

---
+++ RxAndroid_Library/app/src/main/java/com/aclass/shishir/rxandroid_library/MainActivity.java
@@ -0,0 +1,86 @@
+package com.aclass.shishir.rxandroid_library;
+
+import android.app.SearchManager;
+import android.content.Intent;
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.concurrent.Callable;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.observers.DisposableObserver;
+import io.reactivex.schedulers.Schedulers;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "RxAndroidSamples";
+    private TextView button_run_scheduler;
+    private EditText editTextInput;
+    private final CompositeDisposable disposables = new CompositeDisposable();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editTextInput = (EditText) findViewById(R.id.editTextInput);
+
+        button_run_scheduler = (TextView)findViewById(R.id.button_run_scheduler);
+        button_run_scheduler.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                onSearchClick();
+            }
+        });
+    }
+
+    void onRunSchedulerExampleButtonClicked() {
+        disposables.add(sampleObservable()
+                // Run on a background thread
+                .subscribeOn(Schedulers.io())
+                // Be notified on the main thread
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(new DisposableObserver<String>() {
+                    @Override public void onComplete() {
+                        Log.d(TAG, "onComplete()");
+                    }
+
+                    @Override public void onError(Throwable e) {
+                        Log.e(TAG, "onError()", e);
+                    }
+
+                    @Override public void onNext(String string) {
+                        button_run_scheduler.setText("onNext(" + string + ")");
+                        Log.d(TAG, "onNext(" + string + ")");
+                    }
+                }));
+    }
+
+    static Observable<String> sampleObservable() {
+        return Observable.defer(new Callable<ObservableSource<? extends String>>() {
+            @Override public ObservableSource<? extends String> call() throws Exception {
+                // Do some long running operation
+                SystemClock.sleep(5000);
+                return Observable.just("one", "two", "three", "four", "five");
+            }
+        });
+    }
+
+    public void onSearchClick() {
+        try {
+            Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
+            String term = editTextInput.getText().toString();
+            intent.putExtra(SearchManager.SUGGEST_URI_PATH_QUERY, term);
+            startActivity(intent);
+        } catch (Exception e) {
+// TODO: handle exception
+        }
+    }
+}
